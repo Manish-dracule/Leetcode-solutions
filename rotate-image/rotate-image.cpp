b15783c948409fcfa7cf9bb3598b80f565db0334
Manish-dracule <68863157+Manish-dracule@users.noreply.github.com>
2021-01-05 22:13:38 +0530

    Time: 4 ms (55.30%), Space: 7.6 MB (20.57%) - LeetHub

---
+++ rotate-image/rotate-image.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat) {
+        int n=mat.size(),m=mat[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<m;j++)swap(mat[i][j],mat[j][i]);
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m/2;j++)swap(mat[i][j],mat[i][m-j-1]);
+        }
+          
+    }   
+};
