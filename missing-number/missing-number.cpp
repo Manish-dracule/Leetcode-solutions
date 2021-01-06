61702a448775039c73da5b9c7d7c0dd3b9685c3b
Manish-dracule <68863157+Manish-dracule@users.noreply.github.com>
2021-01-06 13:36:12 +0530

    Time: 24 ms (99.43%), Space: 18.3 MB (24.29%) - LeetHub

---
+++ missing-number/missing-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int total=((n+1)*n)/2;
+        int sum=0;
+        for(int i=0;i<n;i++)sum+=nums[i];
+       // cout<<total<<" "<<sum;
+        return total -sum;
+    }
+};
